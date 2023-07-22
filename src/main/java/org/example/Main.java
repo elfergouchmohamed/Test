e73160cab578fcf303645649fb78d372ea88f061
Elfergoucgh Mohamed <melfergouch@gmail.com>
2023-07-22 23:55:03 +0100

    seconde modification

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from jenkins :)");
+        System.out.println("Test 2");
     }
 }
