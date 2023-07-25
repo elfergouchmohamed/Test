556ead620040fba73cda639583f769f1e137712c
Mohamed Elfergouch <64108561+elfergouchmohamed@users.noreply.github.com>
2023-07-25 16:48:08 +0100

    add Test 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from jenkins :)");
         System.out.println("Test 2");
+        System.out.println("Test 3");
     }
 }
