f102c103edd583ce4bf47bc438d84a9b8452fcd2
Mohamed Elfergouch <64108561+elfergouchmohamed@users.noreply.github.com>
2023-08-02 09:36:10 +0100

    update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello from jenkins :)");
-        System.out.println("Test 2");
-        System.out.println("Test 3");
-        System.out.println("Test 4");
+        System.out.println(15+3);
     }
 }
